951f95a6dde5614c3c663733f22809b8c37cba69
anjukrishnaa <141505398+anjukrishnaa@users.noreply.github.com>
2025-02-12 20:34:14 +0530

    Update PrimeNum.java

--- CoreJava/PrimeNum.java
+++ CoreJava/PrimeNum.java
@@ -1,7 +1,7 @@
 import java.util. Scanner;
 public class PrimeNum{
 	public static void main(String args[]){
-		Scanner sc=new Scanner(System.in)
+		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the number");
 		int num=sc.nextInt();
 		boolean flag=false;
