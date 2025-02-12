7a9d67ebafe898e90c7405fffebee4d0b3720305
anjukrishnaa <141505398+anjukrishnaa@users.noreply.github.com>
2025-02-12 20:32:38 +0530

    Update EvenOdd.java

--- CoreJava/EvenOdd.java
+++ CoreJava/EvenOdd.java
@@ -1,6 +1,10 @@
+import java.util Scanner;
 public class EvenOdd{
 	public static void main (String args[]){
-		int n=10;
+		Scanner sc=new Scanner(System.in);
+		System.out.println(System.in);
+		int n= nextInt();
+		
 		if(n%2==0){
 			System.out.println(n+" Number is Even");
 }
